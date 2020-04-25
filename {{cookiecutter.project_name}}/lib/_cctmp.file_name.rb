e12f2765591ce08cfb3860eca1fe85b681c656a5
Ken Collins <ken@metaskills.net>
2020-04-25 16:18:50 -0400

    Simplify lambda.

--- {{cookiecutter.project_name}}/lib/_cctmp.file_name.rb
+++ {{cookiecutter.project_name}}/lib/_cctmp.file_name.rb
@@ -3,10 +3,11 @@ require 'dotenv'
 # Add your gem requires here:
 # require 'some_gem'
 require_relative './{% include "_cctmp/file_name.txt" %}/env'
-require_relative './{% include "_cctmp/file_name.txt" %}/handler'
 # Add your additional lib PORO requires here:
 # require_relative './{% include "_cctmp/file_name.txt" %}/some_poro
 
 def handler(event:, context:)
-  {% include "_cctmp/class_name.txt" %}::Handler.call(event)
+  { statusCode: code,
+    headers: [{'Content-Type' => 'application/json'}],
+    body: JSON.dump(event) }
 end
